ba9fc588308da3542d1fcedd63af966895ee8a8a
James McMichael <tur08816@temple.edu>
2025-10-30 14:46:49 -0400

    Implemented coroutine

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,11 +7,21 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     //TODO (Refactor to replace Thread code with coroutines)
+    //replace handler
+
 
     private val cakeImageView: ImageView by lazy {
         findViewById(R.id.imageView)
@@ -21,24 +31,30 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
 
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
+
+    fun startCoroutineTask() {
+        CoroutineScope(Dispatchers.Default).launch {
+            repeat(100) { index ->
+
+                withContext(Dispatchers.Main) {
+                    currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", index)
+                    cakeImageView.alpha = index / 100f
+                }
+                delay(40) // Non-blocking delay
+            }
+        }
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            GlobalScope.launch { startCoroutineTask() }
         }
     }
 }
